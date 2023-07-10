a87cb12b4d0c3b06ab63c3aa0834edcdb46a79ed
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2023-07-10 16:47:49 +0530

    Json Server and category and sort function

--- src/features/product-List/ProductListApi.js
+++ src/features/product-List/ProductListApi.js
@@ -1,8 +1,25 @@
-// A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>{
-    const reponse = fetch('http://localhost:8080')
-    const data = reponse.json()
+export function fetchAllProducts() {
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
+}
+
+export function fetchProductsByFilters(filter) {
+  // filter = {"category":"smartphone"}
+  // TODO : on server we will support multi values
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
     resolve({data})
   }
   );
